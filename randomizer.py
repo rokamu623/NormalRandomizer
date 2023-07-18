43c578c208ea9679d93810c1c033c8e03f057eff
rokamu623 <r.okamura.061@ms.saitama-u.ac.jp>
2023-07-18 16:30:27 +0900

    feat: randomizer
    Signed-off-by: rokamu623 <r.okamura.061@ms.saitama-u.ac.jp>

---
+++ randomizer.py
@@ -0,0 +1,25 @@
+from numpy import random
+from matplotlib import pyplot
+
+ 
+def two_peak_normal(average: float, standard_deviation: float, outlier_exrate: float, outlier_freqency: float, size: int) -> list[int]:
+    minority_size: int = int(size * outlier_freqency)
+    majority_size: int = size - minority_size
+
+    outputs = list(random.normal(loc=average, scale=standard_deviation, size=majority_size))
+    outputs.extend(list(random.normal(loc=average*outlier_exrate, scale=standard_deviation*outlier_freqency, size=minority_size)))
+
+    outputs = [int(_) for _ in outputs]
+
+    return outputs
+
+def normal(average: float, standard_deviation: float, size: int) -> list[int]:
+    return list(random.normal(loc=average, scale=standard_deviation, size=size))
+
+def data_histgram(data_list: list[int], num_of_divisions: int):
+    hist_tuple = pyplot.hist(data_list, num_of_divisions)
+    return list(hist_tuple[0]), list(hist_tuple[1])
+
+def show_histgram(hist, bins):
+    pyplot.hist(hist, bins)
+    pyplot.show()
